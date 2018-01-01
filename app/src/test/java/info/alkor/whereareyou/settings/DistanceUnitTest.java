0cfe4e645de7a846b024f5d207bfe2060406654b
alkor97 <alkor@konto.pl>
2018-01-01 23:03:30 +0100

    Refactoring: unused imports removed.

--- app/src/test/java/info/alkor/whereareyou/settings/DistanceUnitTest.java
+++ app/src/test/java/info/alkor/whereareyou/settings/DistanceUnitTest.java
@@ -2,44 +2,45 @@ package info.alkor.whereareyou.settings;
 
 import org.junit.Test;
 
-import info.alkor.whereareyou.settings.DistanceUnit;
-
+import static info.alkor.whereareyou.settings.DistanceUnit.FEET;
+import static info.alkor.whereareyou.settings.DistanceUnit.KILOMETERS;
+import static info.alkor.whereareyou.settings.DistanceUnit.METERS;
+import static info.alkor.whereareyou.settings.DistanceUnit.YARDS;
 import static org.junit.Assert.assertEquals;
-import static info.alkor.whereareyou.settings.DistanceUnit.*;
 
 /**
+ * DistanceUnit tests.
  * Created by Marlena on 2017-02-19.
  */
-
 public class DistanceUnitTest {
 
-	private class ValueWithUnit {
-		private final float value;
-		private final DistanceUnit unit;
+    private ValueWithUnit distanceOf(float value, DistanceUnit unit) {
+        return new ValueWithUnit(value, unit);
+    }
 
-		public ValueWithUnit(float value, DistanceUnit unit) {
-			this.value = value;
-			this.unit = unit;
-		}
+    @Test
+    public void test() {
+        distanceOf(0.3048f, METERS).equalsTo(1f, FEET);
+        distanceOf(1f, FEET).equalsTo(0.3048f, METERS);
 
-		public void equalsTo(float value, DistanceUnit unit) {
-			float actual = unit.from(this.value, this.unit);
-			assertEquals(value, actual, 0.01);
-		}
-	}
+        distanceOf(1000f, METERS).equalsTo(1f, KILOMETERS);
+        distanceOf(3f, FEET).equalsTo(1f, YARDS);
 
-	private ValueWithUnit distanceOf(float value, DistanceUnit unit) {
-		return new ValueWithUnit(value, unit);
-	}
+        // TODO: rest of conversions, maybe algorithmic?
+    }
 
-	@Test
-	public void test() {
-		distanceOf(0.3048f, METERS).equalsTo(1f, FEET);
-		distanceOf(1f, FEET).equalsTo(0.3048f, METERS);
+    private class ValueWithUnit {
+        private final float value;
+        private final DistanceUnit unit;
 
-		distanceOf(1000f, METERS).equalsTo(1f, KILOMETERS);
-		distanceOf(3f, FEET).equalsTo(1f, YARDS);
+        ValueWithUnit(float value, DistanceUnit unit) {
+            this.value = value;
+            this.unit = unit;
+        }
 
-		// TODO: rest of conversions, maybe algorithmic?
-	}
+        void equalsTo(float value, DistanceUnit unit) {
+            float actual = unit.from(this.value, this.unit);
+            assertEquals(value, actual, 0.01);
+        }
+    }
 }
