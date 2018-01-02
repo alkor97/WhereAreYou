94e5bcba9fbe3e77cdd1b3c93b607239869c9317
alkor97 <alkor@konto.pl>
2018-01-02 08:29:59 +0100

    SMS receiver tests updated.

--- app/src/test/java/info/alkor/whereareyou/receivers/SmsReceiverTest.java
+++ app/src/test/java/info/alkor/whereareyou/receivers/SmsReceiverTest.java
@@ -7,8 +7,15 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import info.alkor.whereareyou.WhereAreYouContext;
+import info.alkor.whereareyou.model.LocationAction;
 import info.alkor.whereareyou.model.LocationQueryFlowManager;
+import info.alkor.whereareyou.settings.ApplicationSettings;
+import info.alkor.whereareyou.settings.LocationSettings;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -24,21 +31,57 @@ import static org.mockito.Mockito.when;
 public class SmsReceiverTest {
 
     @Mock
-    private WhereAreYouContext context;
+    private WhereAreYouContext mockContext;
 
     @Mock
-    private LocationQueryFlowManager flowManager;
+    private LocationQueryFlowManager mockFlowManager;
+
+    @Mock
+    private LocationAction mockLocationAction;
+
+    @Mock
+    private ApplicationSettings mockApplicationSettings;
+
+    @Mock
+    private LocationSettings mockLockationSettings;
 
     private SmsReceiver receiver = new SmsReceiver();
 
     @Test
     public void ignoreUnrelatedMessages() {
-        when(context.getLocationQueryFlowManager()).thenReturn(flowManager);
-        when(context.getLocationRequestCommand()).thenReturn("Hey, where are you?");
+        when(mockContext.getLocationQueryFlowManager()).thenReturn(mockFlowManager);
+        when(mockContext.getLocationRequestCommand()).thenReturn("Hey, where are you?");
 
-        receiver.onReceive(context, "123", "abc", "random text");
+        receiver.onReceive(mockContext, "123", "abc", "random text");
 
-        verify(flowManager, never()).onIncomingLocationRequest(anyString(), anyString());
-        verify(flowManager, never()).onIncomingLocationResponse(anyString(), anyString(), any(Location.class));
+        verify(mockFlowManager, never()).onIncomingLocationRequest(anyString(), anyString());
+        verify(mockFlowManager, never()).onIncomingLocationResponse(anyString(), anyString(), any(Location.class));
+        verify(mockContext, never()).requestSingleLocationUpdate(anyString(), any(LocationAction.class));
+    }
+
+    @Test
+    public void handleIncomingLocationRequest() {
+        final String command = "Hey, where are you?";
+        final String phone = "123";
+        final String name = "abc";
+        final Set<String> providers = new HashSet<>(Arrays.asList("gps", "network"));
+
+        when(mockContext.getLocationQueryFlowManager()).thenReturn(mockFlowManager);
+        when(mockContext.getLocationRequestCommand()).thenReturn(command);
+        when(mockFlowManager.onIncomingLocationRequest(phone, name)).thenReturn(mockLocationAction);
+        when(mockContext.getApplicationSettings()).thenReturn(mockApplicationSettings);
+        when(mockApplicationSettings.getLocationSettings()).thenReturn(mockLockationSettings);
+        when(mockLockationSettings.getLocationProviders()).thenReturn(providers);
+        for (String provider : providers) {
+            when(mockContext.requestSingleLocationUpdate(provider, mockLocationAction)).thenReturn(true);
+        }
+
+        receiver.onReceive(mockContext, phone, name, command);
+
+        verify(mockFlowManager).onIncomingLocationRequest(phone, name);
+        for (String provider : providers) {
+            verify(mockContext).requestSingleLocationUpdate(provider, mockLocationAction);
+        }
+        verify(mockFlowManager, never()).onIncomingLocationResponse(anyString(), anyString(), any(Location.class));
     }
 }
